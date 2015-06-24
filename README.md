144eff814b4e3b29f76bb9ad9f456daa9a432232
Tan Nhu <tannhu@gmail.com>
2015-06-24 11:22:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+## vdom-renderer
+
+Render HTML content using [Virtual DOM](http://npmjs.org/virtual-dom). This project uses code from [virtual-html](https://github.com/azer/virtual-html) and [html-patcher](https://github.com/azer/html-patcher).
+
+## Build
+
+```bash
+$ browserify index.js > html-renderer.js
+```
+
+## Usage
+
+```html
+<script src="html-renderer.js"></script>
+```
+
+Sample:
+
+```js
+var counter = 0;
+var homeRenderer = htmlRenderer(document.body, renderHome());
+var timeRenderer = htmlRenderer(document.getElementById('time'), renderTime());
+
+setInterval(function () {
+  homeRenderer(renderHome());
+  timeRenderer(renderTime());
+}, 1000);
+
+function renderHome() {
+	counter++;
+  return '<div id="home"><h1>Counter:' + counter + '</h1><div id="time"></div></div>';
+}
+
+function renderInbox() {
+  return '<h1>The time now is:<span>' + new Date().toGMTString() + '</span></h1>';
+}
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,142 @@
+var createVNode = require('virtual-dom/h');
+var svg = require('virtual-dom/virtual-hyperscript/svg');
+var htmltree = require("htmltree");
+var createRootNode = require('virtual-dom/create-element');
+var diff = require('virtual-dom/diff');
+var applyPatches = require('virtual-dom/patch');
+
+// @see https://github.com/azer/virtual-html
+function virtualHTML (html, callback) {
+  callback = callback || defaultCb;
+  if (typeof html == 'function') html = html();
+  var res = null;
+
+  htmltree(html, function (err, dom) {
+    if (err) return callback(err);
+    res = vnode(dom.root[0]);
+    callback(undefined, res);
+  });
+
+  return res;
+
+  function defaultCb (err) {
+    if (err) throw new Error(err);
+  }
+}
+
+function vnode(parent, svgParent) {
+  if (parent.type == "text") return parent.data;
+  if (parent.type != "tag") return;
+
+  var isSVG = parent.name === 'svg' || svgParent;
+  var children;
+  var child;
+  var len;
+  var i;
+
+  if (parent.children.length) {
+    children = [];
+    len = parent.children.length;
+    i = -1;
+
+    while (++i < len) {
+      child = vnode(parent.children[i], isSVG);
+      if (!child) continue;
+      children.push(child);
+    }
+  }
+
+  var properties = createProperties(parent.attributes);
+  if (isSVG) {
+    return svg(parent.name, properties, children, true);
+  }
+  return createVNode(parent.name, properties, children);
+}
+
+function style (raw) {
+  if (!raw) return {};
+
+  var result = {};
+  var fields = raw.split(/;\s?/);
+  var len = fields.length;
+  var i = -1;
+  var s;
+  var field;
+
+  while (++i < len) {
+    field = fields[i].trim();
+    if (!field) continue;
+    s = field.indexOf(':');
+    result[field.slice(0, s)] = field.slice(s + 1).trim();
+  }
+
+  return result;
+}
+
+function createProperties (attributes) {
+  var properties = {};
+  var attrs = {};
+
+  for (var key in attributes) {
+    if (!attributes.hasOwnProperty(key)) continue;
+
+    switch (key) {
+    case 'style':
+      properties.style = style(attributes.style);
+      break;
+    case 'class':
+      properties.className = attributes['class'];
+      break;
+    case 'for':
+      properties.htmlFor = attributes['for'];
+      break;
+    default:
+      if (key.indexOf('data-') === 0) {
+        addDataSet(properties, key, attributes[key]);
+      } else {
+        attrs[key] = attributes[key];
+      }
+    }
+  }
+
+  properties.attributes = attrs;
+
+  return properties;
+}
+
+function addDataSet (properties, key, value) {
+  properties.dataset || (properties.dataset = {});
+  properties.dataset[camel(key.slice(5))] = value;
+}
+
+// @see https://github.com/azer/html-patcher
+function patcher(parentNode, html) {
+  var tree;
+  var rootNode;
+
+  patch(html);
+
+  return patch;
+
+  function patch (newHtml) {
+    var vdom = virtualHTML(newHtml || html);
+
+    if (!tree) {
+      tree = vdom;
+      rootNode = createRootNode(vdom);
+      parentNode.appendChild(rootNode);
+      return patch;
+    }
+
+    var patches = diff(tree, vdom);
+    applyPatches(rootNode, patches);
+    tree = vdom;
+
+    return patch;
+  }
+}
+
+module.exports = patcher;
+if (typeof(window) !== 'undefined') {
+  window.htmlRenderer = patcher;
+}
